749261690ab7dfa85222d36c495283bd0664b385
thatipiresss <thaticmg@gmail.com>
2022-02-23 22:22:35 -0300

    ajustando api

--- blog-pessoal/src/services/Service.ts
+++ blog-pessoal/src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://bloggeneration.herokuapp.com'
+    baseURL: 'https://blogdathatizap.herokuapp.com'
 })
 
 export const login = async(url:any, dados:any, setDado:any) => {
